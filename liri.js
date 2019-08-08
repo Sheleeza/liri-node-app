45dde04943605e3fb982f89d9d5dee09aac6ee9f
Sheleeza <sheleezar9@gmail.com>
2019-08-08 16:59:56 -0400

    edit js

--- liri.js
+++ liri.js
@@ -1,14 +1,10 @@
 require("dotenv").config();
 var axios = require("axios");
+var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
-var moment = require("moment");
-var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
-
-<<<<<<< HEAD
-// var command = process.argv[2]
-// var command3 = process.argv[3]
-
+var moment = require('moment');
+var fs = require("fs");
 
 switch(process.argv[2]){
     case 'concert-this':
@@ -111,52 +107,3 @@ function whatever(){
       
       });
 };
-=======
-var command = process.argv[2]
-var command3 = process.argv[3]
-
-switch (command) {
-    case "concert-this":
-        concert();
-        break;
-
-    case "spotify-this-song":
-        spotifythis();
-        break;
-        
-    case "movie-this":
-        movie();
-        break;
-
-    case "do-what-it-says":
-        dowhatitsays();
-        break;    
-}
-
-
-//concert this add searchs render events
-function concert() {
-   var queryUrl = "https://rest.bandsintown.com/artists/" + command3 + "/events?app_id=codingbootcamp"
-
-   axios.get(gueryUrl).then(
-       function(response) {
-        console.log(response);
-       })
-       .catch(function(error) {
-        console.log(error);
-       })
-       
-}
-
-function spotify()
-
-// spotify
-//   .search({ type: 'artist OR album OR track', query: 'All the Small Things' })
-//   .then(function(response) {
-//     console.log(response);
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   });
-
->>>>>>> 1c65da0a92328e8771b01901d4d6fdb4742a8dab
